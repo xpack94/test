53529facebe534ebafe953d02a9b2145219d41a2
abdesselam <xpack94@gmail.com>
2026-05-04 20:23:06 -0400

    add new multiply and subsctract functions

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const sum = (number1, number2) => {
   return number1 + number2;
 };
+
+const subtract = (number1, number2) => {
+  return number1 - number2;
+};
+
+const multiply = (number1, number2) => {
+  return number1 * number2;
+};
